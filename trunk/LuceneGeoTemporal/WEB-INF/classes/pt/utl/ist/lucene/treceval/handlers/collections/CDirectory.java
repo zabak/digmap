949627b12eb907cfc9fc31d3f4399816855ee990
machadofisher <machadofisher@8a765013-e053-0410-9ef8-2dfa22d392dd>
2009-10-14 14:03:01 +0000

    FrbrDigmap

--- trunk/LuceneGeoTemporal/WEB-INF/classes/pt/utl/ist/lucene/treceval/handlers/collections/CDirectory.java
+++ trunk/LuceneGeoTemporal/WEB-INF/classes/pt/utl/ist/lucene/treceval/handlers/collections/CDirectory.java
@@ -5,6 +5,7 @@ import org.dom4j.DocumentException;
 import pt.utl.ist.lucene.treceval.Globals;
 import pt.utl.ist.lucene.treceval.IndexFilesCallBack;
 import pt.utl.ist.lucene.treceval.handlers.ResourceHandler;
+import pt.utl.ist.lucene.utils.Files;
 
 import java.io.*;
 import java.net.MalformedURLException;
@@ -43,26 +44,35 @@ public class CDirectory implements ICollectionPreprocessor
         int i = 0;
         for (File f : files)
         {
-            logger.info("directory> handling (" + i + "): " + f.getName());
-            CDocumentHandler documentHandler = CFileTypeFactory.createHandler(f.getName(),fileTypesPlugins);
-            try
+            if(f.isDirectory())
             {
-                InputStream fileInputStream = new FileInputStream(f);
-                if(collectionPath.startsWith(Globals.DATA_DIR))
-                    documentHandler.handle(fileInputStream, collectionPath.substring(Globals.DATA_DIR.length()) + File.separator + f.getName(), resourceHandler,callBack, fileTypesPlugins);
-                else
-                    documentHandler.handle(fileInputStream, collectionPath + File.separator + f.getName(), resourceHandler,callBack, fileTypesPlugins);
-                fileInputStream.close();
+                handle(f.getAbsolutePath(),callBack);
             }
-            catch (FileNotFoundException e)
+            else
             {
-                logger.error(e,e);
+                logger.info("directory> handling (" + i + "): " + f.getName());
+                CDocumentHandler documentHandler = CFileTypeFactory.createHandler(f.getName(),fileTypesPlugins);
+                try
+                {
+                    collectionPath = Files.normalizePathSeparators(collectionPath);
+                    Globals.DATA_DIR = Files.normalizePathSeparators(Globals.DATA_DIR);
+                    InputStream fileInputStream = new FileInputStream(f);
+                    if(collectionPath.startsWith(Globals.DATA_DIR))
+                        documentHandler.handle(fileInputStream, collectionPath.substring(Globals.DATA_DIR.length()) + File.separator + f.getName(), resourceHandler,callBack, fileTypesPlugins);
+                    else
+                        documentHandler.handle(fileInputStream, collectionPath + File.separator + f.getName(), resourceHandler,callBack, fileTypesPlugins);
+                    fileInputStream.close();
+                }
+                catch (FileNotFoundException e)
+                {
+                    logger.error(e,e);
+                }
+                catch (IOException e)
+                {
+                    logger.error(e,e);
+                }
+                i++;
             }
-            catch (IOException e)
-            {
-                logger.error(e,e);
-            }
-            i++;
         }
     }
 }

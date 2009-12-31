df78771df5928713f74f6a66eed868eaa33f8621
machadofisher <machadofisher@8a765013-e053-0410-9ef8-2dfa22d392dd>
2009-12-31 13:37:32 +0000

    refactoring the structure of geotime package

---
+++ LuceneGeoTemporal/WEB-INF/classes/pt/utl/ist/lucene/treceval/geotime/utiltasks/DocumentTagger.java
@@ -0,0 +1,163 @@
+package pt.utl.ist.lucene.treceval.geotime.utiltasks;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.File;
+
+import pt.utl.ist.lucene.treceval.geotime.NyTimesDocument;
+
+import pt.utl.ist.lucene.treceval.geotime.webservices.CallWebServices;
+import pt.utl.ist.lucene.treceval.geotime.DocumentIterator;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+
+/**
+ * @author Jorge Machado
+ * @date 31/Dez/2009
+ * @time 13:27:29
+ * @email machadofisher@gmail.com
+ */
+public class DocumentTagger
+{
+    private static final Logger logger = Logger.getLogger(DocumentTagger.class);
+    
+    public static void main(String[]args) throws IOException
+    {
+
+        String path = args[0];
+        String mode = args[1];
+
+//          String path = "D:\\Servidores\\DATA\\ntcir\\nyt_eng_200509.gz";
+        /**
+         *
+         * advis
+         nyt_eng   512   81988  142193
+         multi
+         nyt_eng   124   20435   33183
+         other
+         nyt_eng   109   16664   25597
+         story
+         nyt_eng  6304 1069406 1454306
+         */
+        int count = 0;
+        int advis = 0;
+        int multi = 0;
+        int other = 0;
+        int story = 0;
+        int unknown = 0;
+        int EasternCount = 0;
+        int CenturiesAgo = 0;
+        int MilleniaAgo = 0;
+        int ok = 0;
+        int fail = 0;
+        String startId = null;
+        if(mode.equals("PlaceMaker") && args.length > 2)
+            startId = args[2];
+        else if(mode.equals("timextag") && args.length > 3)
+            startId = args[3];
+
+        DocumentIterator di = new DocumentIterator(path);
+        NyTimesDocument d;
+        FileOutputStream out;
+        String file;
+        if(new File(path).isFile())
+            file = path + "_notes01.xml";
+        else
+            file = path + "/notes01.xml";
+        out = new FileOutputStream(file,false);
+
+        System.out.println("output file: " + file);
+        out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".getBytes());
+        out.write("<docs>\n".getBytes());
+
+        while((d = di.next()) != null)
+        {
+            System.out.println(count + ":" + d.getDId());
+            logger.warn(count + ":" + d.getDId());
+            count++;
+
+            boolean go = false;
+
+            if(MultiDocumentTagger.failIds != null)
+            {
+                if(MultiDocumentTagger.failIds.contains(d.getDId()))
+                    go = true;
+            }
+            else if( startId==null || d.getDId().compareTo(startId) >= 0)
+                go = true;
+
+            if( go )
+            {
+                if(d.getSgml().toLowerCase().indexOf("Eastern")>0)
+                    EasternCount++;
+                if(d.getSgml().toLowerCase().indexOf("millennia ago")>0)
+                    MilleniaAgo++;
+                if(d.getSgml().toLowerCase().indexOf("centuries ago")>0)
+                    CenturiesAgo++;
+
+                startId = null;
+                if((d.getDParagraphs() == null || d.getDParagraphs().size() == 0) && (d.getDText() == null || d.getDText().trim().length() == 0))
+                    d.printOut();
+                if(d.getDType().equals("story"))
+                    story++;
+                else if(d.getDType().equals("advis"))
+                    advis++;
+                else if(d.getDType().equals("multi"))
+                    multi++;
+                else if(d.getDType().equals("other"))
+                    other++;
+                else
+                    unknown++;
+                logger.debug("Calling with:" + d.toString());
+                out.write(("<doc id=\"" + d.getDId() + "\">\n").getBytes());
+                try {
+
+                    org.w3c.dom.Document dxml = null;
+                    if(mode.equals("PlaceMaker"))
+                        dxml = CallWebServices.callServices(d.toString().replace("&AMP;","&amp;"),d.getDHeadline(),d.getArticleYear(),d.getArticleMonth(), d.getArticleDay(),d.getFSourceFile(),d.getDId());
+                    else if(mode.equals("timextag"))
+                    {
+                        String url = args[2];
+                        dxml = CallWebServices.callTimextag(url,d.getSgml(),d.getDHeadline(),d.getArticleYear(),d.getArticleMonth(),d.getArticleDay(),d.getFSourceFile(),d.getDId());
+                    }
+                    ok++;
+                    OutputFormat of = new OutputFormat("XML","UTF-8",true);
+                    of.setIndent(1);
+                    of.setOmitXMLDeclaration(true);
+                    of.setIndenting(true);
+                    XMLSerializer serializer = new XMLSerializer(out,of);
+                    serializer.asDOMSerializer();
+                    serializer.serialize( dxml.getDocumentElement() );
+                } catch (Throwable e) {
+                    logger.error(d.getDId() +  "@" + path +": " + e.toString(),e);
+                    fail++;
+                }
+                out.write(("</doc>\n").getBytes());
+            }
+            else
+            {
+                System.out.println("Skiping: " + d.getDId());
+            }
+        }
+        out.write("</docs>\n".getBytes());
+        out.flush();
+        out.close();
+
+        logger.fatal("******************************************************************************");
+        logger.fatal("STATUS FOR FILE: " + file);
+        logger.fatal("OK: " + ok);
+        logger.fatal("FAIL: " + fail);
+        logger.fatal("count: " + count);
+        logger.fatal("story: " + story);
+        logger.fatal("advis: " + advis);
+        logger.fatal("multi: " + multi);
+        logger.fatal("other: " + other);
+        logger.fatal("unknown: " + unknown);
+        logger.fatal("Eastern word: " + EasternCount);
+        logger.fatal("millennia ago: " + MilleniaAgo);
+        logger.fatal("centuries ago: " + CenturiesAgo);
+        logger.fatal("******************************************************************************");
+    }
+}
---
+++ LuceneGeoTemporal/WEB-INF/classes/pt/utl/ist/lucene/treceval/geotime/utiltasks/TimexesStatistics.java
@@ -0,0 +1,209 @@
+package pt.utl.ist.lucene.treceval.geotime.utiltasks;
+
+import org.apache.log4j.Logger;
+import pt.utl.ist.lucene.treceval.geotime.IntegratedDocTimexIterator;
+import pt.utl.ist.lucene.utils.temporal.TimeExpression;
+import pt.utl.ist.lucene.utils.temporal.tides.Timex2TimeExpression;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Jorge Machado
+ * @date 29/Dez/2009
+ * @time 20:06:34
+ * @email machadofisher@gmail.com
+ */
+public class TimexesStatistics {
+
+    private static final Logger logger = Logger.getLogger(TimexesStatistics.class);
+
+
+
+    public static void main(String[]args) throws IOException
+    {
+        FileWriter fw = new FileWriter("d:\\tmp\\statsTotals.xml",false);
+        fw.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+        fw.write("<statsNtcirTemporal>\n");
+
+
+        Map<String,Integer> expressionsTypeMonth = new HashMap<String,Integer>();
+        Map<String,Integer> expressionsTypeTotals = new HashMap<String,Integer>();
+
+        int docs = 0;
+        int docsWithTimexes = 0;
+        int numberTimexes = 0;
+        int numberTemporalExprs = 0;
+        int invalidExpressions = 0;
+
+
+
+        IntegratedDocTimexIterator documentTimexesIterator = new IntegratedDocTimexIterator("D:\\Servidores\\DATA\\ntcir\\TEMPORAL\\teste\\docs","D:\\Servidores\\DATA\\ntcir\\TEMPORAL\\teste\\timexes");
+
+        String previousID = "";
+        IntegratedDocTimexIterator.DocumentWithTimexes d;
+
+        int docsMonth = 0;
+        int docsWithTimexesMonth = 0;
+        int numberTimexesMonth = 0;
+        int numberTemporalExprsMonth = 0;
+        int invalidExpressionsMonth = 0;
+
+        while((d = documentTimexesIterator.next())!=null)
+        {
+//            logger.fatal(d.getD().getDId());
+            if(previousID.length() > 0 && !previousID.substring(0,14).equals(d.getD().getDId().substring(0,14)))
+            {
+
+                logger.fatal("###############################################");
+                logger.fatal("# stat: File: " + previousID.substring(0,14));
+                logger.fatal("# stat: docsMonth:" + docsMonth);
+                logger.fatal("# stat: docsWithTimexes:" + docsWithTimexesMonth);
+                logger.fatal("# stat: numberTimexes:" + numberTimexesMonth);
+                logger.fatal("# stat: numberTemporalExprs:" + numberTemporalExprsMonth);
+                logger.fatal("# stat: invalidExpressions:" + invalidExpressionsMonth);
+                logger.fatal("###############################################");
+                fw.write("<file name=\"" + previousID.substring(0,14) + "\">\n");
+                fw.write("<stat name=\"docs\">" + docsMonth + "</stat>\n");
+                fw.write("<stat name=\"docsWithTimexesMonth\">" + docsWithTimexesMonth + "</stat>\n");
+                fw.write("<stat name=\"numberTimexesMonth\">" + numberTimexesMonth + "</stat>\n");
+                fw.write("<stat name=\"numberTemporalExprsMonth\">" + numberTemporalExprsMonth + "</stat>\n");
+                fw.write("<stat name=\"invalidExpressionsMonth\">" + invalidExpressionsMonth + "</stat>\n");
+                printTimexes(expressionsTypeMonth,fw);
+                fw.write("</file>");
+                fw.flush();
+                docsMonth = 0;
+                docsWithTimexesMonth = 0;
+                numberTimexesMonth = 0;
+                numberTemporalExprsMonth = 0;
+                invalidExpressionsMonth = 0;
+                expressionsTypeMonth.clear();
+            }
+            previousID = d.getD().getDId();
+            docs++;
+            docsMonth++;
+            if(d.hasTimexes())
+            {
+                docsWithTimexes++;
+                docsWithTimexesMonth++;
+            }
+            else if(d.getTd() == null || d.getTd().getTimex2TimeExpressions() == null || d.getTd().getTimex2TimeExpressions().size() == 0)
+            {
+                logger.error(d.getD().getDId() + ": FAILED Timexes anotation for Document with 0 timexes");
+            }
+            else
+                logger.error(d.getD().getDId() + ": " + d.countTimexes() + " Timexes " );
+            numberTimexes+=d.countTimexes();
+            numberTimexesMonth+=d.countTimexes();
+
+            numberTemporalExprs+=d.countTimeExpressions();
+            numberTemporalExprsMonth+=d.countTimeExpressions();
+
+            int invalid = d.countInvalidTimeExpressions();
+            invalidExpressions += invalid;
+            invalidExpressionsMonth += invalid;
+            if(invalid > 0)
+            {
+                logger.error("Document: " + d.getD().getDId() + " has invalid TimeExpressions");
+                for(TimeExpression invalidTimeExpression : d.getTd().getAllInvalidTimeExpressions())
+                {
+                    logger.error(invalidTimeExpression.getNormalizedExpression() + ": " + invalidTimeExpression.getValidationError() + " context: " + invalidTimeExpression.getRefNLTxt());
+
+                }
+            }
+
+            /**
+             * Stats Expressions
+             */
+            if(d.hasTimexes() && d.getTd().getAllTimeExpressions() != null)
+            {
+                for(Timex2TimeExpression timex2TimeExpression: d.getTd().getTimex2TimeExpressions())
+                {
+                    if(timex2TimeExpression.getTimex2().getType().isP())
+                    {
+                        String val = timex2TimeExpression.getTimex2().getType().name();
+                        String anchorVal = timex2TimeExpression.getTimex2().getTypeAnchorVal().name();
+                        if(anchorVal == null)
+                            anchorVal = "NULL";
+                        String anchorDir = timex2TimeExpression.getTimex2().getAnchorDir();
+                        if(anchorDir == null)
+                            anchorDir = "NULL";
+                        String key = val + "$$" + anchorVal + "$$" + anchorDir;
+                        Integer count = expressionsTypeMonth.get(key);
+                        if(count == null)
+                            count = 0;
+                        expressionsTypeMonth.put(key,count+1);
+
+                        Integer countTotal = expressionsTypeTotals.get(key);
+                        if(countTotal == null)
+                            countTotal = 0;
+                        expressionsTypeTotals.put(key,countTotal+1);
+                    }
+                    else
+                    {
+                        //KEY: ExpressionType-Month VAL: Count
+                        String key = timex2TimeExpression.getTimex2().getType().name();
+                        Integer count = expressionsTypeMonth.get(key);
+                        if(count == null)
+                            count = 0;
+                        expressionsTypeMonth.put(key,count+1);
+
+
+                        Integer countTotal = expressionsTypeTotals.get(key);
+                        if(countTotal == null)
+                            countTotal = 0;
+                        expressionsTypeTotals.put(key,countTotal+1);
+                    }
+                }
+            }
+
+        }
+
+        logger.fatal("###############################################");
+        logger.fatal("# stat: docs:" + docs);
+        logger.fatal("# stat: docsWithTimexes:" + docsWithTimexes);
+        logger.fatal("# stat: numberTimexes:" + numberTimexes);
+        logger.fatal("# stat: numberTemporalExprs:" + numberTemporalExprs);
+        logger.fatal("# stat: invalidExpressions:" + invalidExpressions);
+        logger.fatal("###############################################");
+        fw.write("<totals>\n");
+        fw.write("<stat name=\"docs\">" + docs + "</stat>\n");
+        fw.write("<stat name=\"docsWithTimexes\">" + docsWithTimexes + "</stat>\n");
+        fw.write("<stat name=\"numberTimexes\">" + numberTimexes + "</stat>\n");
+        fw.write("<stat name=\"numberTemporalExprs\">" + numberTemporalExprs + "</stat>\n");
+        fw.write("<stat name=\"invalidExpressions\">" + invalidExpressions + "</stat>\n");
+
+        printTimexes(expressionsTypeTotals,fw);
+        fw.write("</totals>");
+        fw.write("</statsNtcirTemporal>\n");
+        fw.flush();
+        fw.close();
+
+    }
+
+    private static void printTimexes(Map<String,Integer> expressionsMap, FileWriter fw) throws IOException {
+        fw.write("<temporal_expressions>\n");
+        for(Map.Entry<String,Integer> entry: expressionsMap.entrySet())
+        {
+            String val = entry.getKey();
+
+            Integer count = entry.getValue();
+            if(val.indexOf("$$")>0)
+            {
+                String[] vals = val.split("\\$\\$");
+                val = vals[0];
+                String anchor_val= vals[1];
+                String anchor_dir= vals[2];
+
+                fw.write("<timex2 type=\"p\" val=\"" + val + "\" anchor_val=\"" + anchor_val + "\" anchor_dir=\"" + anchor_dir + "\" count=\"" + count + "\"/>");
+            }
+            else
+            {
+                fw.write("<timex2 val=\"" + val + "\" count=\"" + count + "\"/>");
+            }
+        }
+        fw.write("</temporal_expressions>\n");
+    }
+}
